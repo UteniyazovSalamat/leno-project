3c5c6fe5c392e3aecc047eb2444b22791c202a07
UteniyazovSalamat <sss19855891@gmail.com>
2025-07-01 13:08:34 -0600

    Add desktop and mobile versions of Preview and Modal sections, and styles

--- js/main.js
+++ js/main.js
@@ -1,12 +1,38 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Mobile Menu
     const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
     toggleButton.addEventListener('click', function () {
         mobileMenu.classList.toggle('active');
     });
+
+    // Video Modal
+    const modal = document.getElementById('videoModal');
+    const videoButton = document.querySelector('.preview__video-button');
+    const closeButton = document.querySelector('.modal__close-button');
+    const videoPlayer = document.getElementById('videoPlayer');
+
+    videoButton.addEventListener('click', function () {
+        modal.style.display = 'block';
+
+        videoPlayer.src = 'https://www.youtube.com/embed/fUjbj3sNWuU';
+
+        closeButton.addEventListener('click', function () {
+            modal.style.display = 'none';
+            videoPlayer.src = '';
+        });
+
+        window.addEventListener('click', function (e) {
+            if (e.target == modal) {
+                modal.style.display = 'none';
+                videoPlayer.src = '';
+            }
+        });
+    });
 });
 
+// Navigation background on scroll
 window.addEventListener('scroll', function () {
     const navbar = this.document.querySelector('.navbar');
 
